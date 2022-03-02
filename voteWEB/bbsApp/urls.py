8b269da0ffb62fa42a970d4261d2654d73af9c5e
Eseul Lee <dltmf3401@gmail.com>
2022-03-02 17:03:24 +0900

    ^-^

--- voteWEB/bbsApp/urls.py
+++ voteWEB/bbsApp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
-from bbsApp import views
+from django.urls    import path,include
+from bbsApp         import views
+
+
 urlpatterns = [
     path('index/', views.index, name = 'bbs_index'),
 
