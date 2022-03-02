43c55dd29d55e06b0742506b80fa96996fbeba90
Wonsung2 <q92235@naver.com>
2022-03-02 17:07:49 +0900

    login

--- voteWEB/bbsApp/urls.py
+++ voteWEB/bbsApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from bbsApp import views
 urlpatterns = [
     path('index/', views.index, name = 'bbs_index'),
+    path('login/', views.login, name = 'bbs_login'),
 
 
 ]
