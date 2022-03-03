295ce993040b5d55746e791cfaec95a969f6f490
Jason <jaewoo.choi@shms-mail.ch>
2022-03-03 14:01:44 +0900

    joinFrom is done!

--- voteWEB/bbsApp/urls.py
+++ voteWEB/bbsApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('index/', views.index, name = 'bbs_index'),
     path('login/', views.login, name = 'bbs_login'),
     path('joinForm/', views.joinForm, name = 'joinForm'),
+    path('join/',   views.join, name='join'),
+
 
 ]
--- voteWEB/bbsApp/views.py
+++ voteWEB/bbsApp/views.py
@@ -9,5 +9,21 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
+
 def joinForm(request) :
     return render(request, 'joinForm.html')
+
+def join(request):
+    print('>>>> user join - ')
+    id = request.POST['id']
+    pwd = request.POST['pwd']
+    name = request.POST['name']
+    poli = request.POST['poli']
+    age = request.POST['age']
+
+    print('>>>> param values - ', id, pwd, name,poli,age)
+        # insert into table(id, pwd, name) values('','','')
+        # orm : modelName(attr - value).save()
+    WebMember(member_id=id, member_pwd=pwd, member_name=name, member_poli=poli,member_age=age).save()
+    # return render(request , 'user/index.html')
+    return redirect('bbs_index')
