155dc3ef3f18f56dc848013bb595ebb17277b677
Jason <jaewoo.choi@shms-mail.ch>
2022-03-07 17:00:01 +0900

    1. candidate1 is done

--- voteWEB/candidate1App/views.py
+++ voteWEB/candidate1App/views.py
@@ -4,5 +4,26 @@ from django.shortcuts import render, redirect
 
 def candidate1(request):
     return render(request,'candidate1/candidate1.html')
-def detail(request):
-    return render(request, 'candidate1/blog-two-details')
+def detail01(request):
+    return render(request, 'candidate1/blogDetail1.html')
+def detail02(request):
+    return render(request, 'candidate1/blogDetail2.html')
+def detail03(request):
+    return render(request, 'candidate1/blogDetail3.html')
+def detail04(request):
+    return render(request, 'candidate1/blogDetail4.html')
+def detail05(request):
+    return render(request, 'candidate1/blogDetail5.html')
+def detail06(request):
+    return render(request, 'candidate1/blogDetail6.html')
+def detail07(request):
+    return render(request, 'candidate1/blogDetail7.html')
+def detail08(request):
+    return render(request, 'candidate1/blogDetail8.html')
+def detail09(request):
+    return render(request, 'candidate1/blogDetail9.html')
+def detail10(request):
+    return render(request, 'candidate1/blogDetail10.html')
+
+def detail01(request) :
+    return render(request, 'candidate1/blogDetail1.html')
