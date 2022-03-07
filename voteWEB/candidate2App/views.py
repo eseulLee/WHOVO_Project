974d60e98f06c15e7fd3a69ff03a8cc7df0ea98e
Eseul Lee <dltmf3401@gmail.com>
2022-03-07 16:14:08 +0900

    blogdetail_윤석열

--- voteWEB/candidate2App/views.py
+++ voteWEB/candidate2App/views.py
@@ -4,3 +4,33 @@ from django.shortcuts import render, redirect
 
 def candidate2(request):
     return render(request, 'candidate2/candidate2.html')
+
+def detail01(request) :
+    return render(request, 'candidate2/blogDetail1.html')
+
+def detail02(request) :
+    return render(request, 'candidate2/blogDetail2.html')
+
+def detail03(request) :
+    return render(request, 'candidate2/blogDetail3.html')
+
+def detail04(request) :
+    return render(request, 'candidate2/blogDetail4.html')
+
+def detail05(request) :
+    return render(request, 'candidate2/blogDetail5.html')
+
+def detail06(request) :
+    return render(request, 'candidate2/blogDetail6.html')
+
+def detail07(request) :
+    return render(request, 'candidate2/blogDetail7.html')
+
+def detail08(request) :
+    return render(request, 'candidate2/blogDetail8.html')
+
+def detail09(request) :
+    return render(request, 'candidate2/blogDetail9.html')
+
+def detail10(request) :
+    return render(request, 'candidate2/blogDetail10.html')
