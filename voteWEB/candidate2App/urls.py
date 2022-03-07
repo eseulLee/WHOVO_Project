fddd73a308ce98257eb89e42d065d4a4fb7ddd95
Eseul Lee <dltmf3401@gmail.com>
2022-03-07 16:24:22 +0900

    urls & views

--- voteWEB/candidate2App/urls.py
+++ voteWEB/candidate2App/urls.py
@@ -3,5 +3,15 @@ from candidate2App import views
 
 urlpatterns = [
     path('index/', views.candidate2),
-    path('detail/', views.detail),
+    path('detail01/', views.detail01),
+    path('detail02/', views.detail02),
+    path('detail03/', views.detail03),
+    path('detail04/', views.detail04),
+    path('detail05/', views.detail05),
+    path('detail06/', views.detail06),
+    path('detail07/', views.detail07),
+    path('detail08/', views.detail08),
+    path('detail09/', views.detail09),
+    path('detail10/', views.detail10),
+
 ]
--- voteWEB/candidate2App/views.py
+++ voteWEB/candidate2App/views.py
@@ -4,5 +4,33 @@ from django.shortcuts import render, redirect
 
 def candidate2(request):
     return render(request, 'candidate2/candidate2.html')
-def detail(request):
-    return render(request, 'candidate1/blog-two-details')
+
+def detail01(request):
+    return render(request, 'candidate2/blogDetail01.html')
+
+def detail02(request):
+    return render(request, 'candidate2/blogDetail02.html')
+
+def detail03(request):
+    return render(request, 'candidate2/blogDetail03.html')
+
+def detail04(request):
+    return render(request, 'candidate2/blogDetail04.html')
+
+def detail05(request):
+    return render(request, 'candidate2/blogDetail05.html')
+
+def detail06(request):
+    return render(request, 'candidate2/blogDetail06.html')
+
+def detail07(request):
+    return render(request, 'candidate2/blogDetail07.html')
+
+def detail08(request):
+    return render(request, 'candidate2/blogDetail08.html')
+
+def detail09(request):
+    return render(request, 'candidate2/blogDetail09.html')
+
+def detail10(request):
+    return render(request, 'candidate2/blogDetail10.html')
