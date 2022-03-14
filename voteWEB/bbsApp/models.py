63b97d04346235ae36dd13b9782d09afe6b340cd
Eseul Lee <dltmf3401@gmail.com>
2022-03-14 11:15:45 +0900

    models

--- voteWEB/bbsApp/models.py
+++ voteWEB/bbsApp/models.py
@@ -17,15 +17,16 @@ class WebMember(models.Model):
 class Post(models.Model):
     # id(pk), writer_id, writer_name, content, regdate, viewcnt
     id            = models.BigAutoField(help_text = "Post ID", primary_key=True)
-    candidate_num = models.IntegerField
-    detail_num    = models.IntegerField
+    candidate_num = models.IntegerField()
+    detail_num    = models.IntegerField()
     writer_id     = models.ForeignKey("userApp.WebMember", on_delete=models.CASCADE, db_column='writer_id', help_text= "post writer ID")
     content       = models.TextField(blank=False, null=False)
     create_date   = models.DateTimeField(auto_now=True)
     modify_date   = models.DateTimeField(auto_now=True)
-
+'''
     def __str__(self):
         return self.id+"\t"+self.writer_id
+'''
 
 class Comment(models.Model):
     # id(pk), contents, writer_id, writer_name, bbs_id(fk)
@@ -35,8 +36,9 @@ class Comment(models.Model):
     contents     = models.TextField(blank=False, null=False)
     create_date  = models.DateTimeField(auto_now=True)
     modify_date  = models.DateTimeField(auto_now=True)
-
+'''
     def __str__(self):
         return self.id+"\t"+self.writer_id
+'''
 
 # post, comment 둘다 좋아요 수 미기재
