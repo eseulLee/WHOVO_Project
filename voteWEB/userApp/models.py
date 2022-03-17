18e2815f25a85efb903d9f52735ef4a2c1c116f8
Eseul Lee <dltmf3401@gmail.com>
2022-03-17 14:59:28 +0900

    차트데이터 만들기 실패..

--- voteWEB/userApp/models.py
+++ voteWEB/userApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 
@@ -9,3 +10,10 @@ class WebMember(models.Model):
     member_poli = models.TextField(max_length=100, blank=False, null=False)
     member_age = models.DateField()
     member_regdate = models.DateTimeField(auto_now=True)
+
+    # def ageCalc(self, member_age):
+    #     birth_year = member_age.dt.date.year
+    #     today = dt.date.today().year
+    #     age_calc = today - birth_year + 1
+    #     return age_calc
+
--- voteWEB/voteWEB/views.py
+++ voteWEB/voteWEB/views.py
@@ -1,23 +1,67 @@
 from django.shortcuts import render
 from bbsApp.models import Post
 from userApp.models import WebMember
+import datetime
 
 def index(request) :
     print('>>> whovo')
+    # lst1 : 30대 이하 정당 지지도 / lst2 : 4050 정당 지지도 / lst3 : 60대 이상 정당 지지도
+    lst1 = []
+    lst2 = []
+    lst3 = []
+
+    # users_age = WebMember.objects.values('member_age')
+    # users_poli = WebMember.objects.values('member_poli')
+    # print(users_age)
+
+    # for user in users:
+    #     birth_year = user['member_age'].year
+    #     today_year = datetime.date.today().year
+    #     mem_age = today_year - birth_year + 1
+    #     if mem_age <= 39:
+    #
+    #         lst1.append(mem_age)
+    #     elif mem_age <= 59:
+    #         lst2.append(mem_age)
+    #     else:
+    #         lst3.append(mem_age)
+    # 선호정당 별 user 구분 (4)
+    cand1_users = WebMember.objects.filter(member_poli = '더불어민주당')
+    cand2_users = WebMember.objects.filter(member_poli = '국민의힘')
+    cand3_users = WebMember.objects.filter(member_poli = '정의당')
+    for user in cand1_users :
+        # birth = user.values('member_age')
+        print(user.objects.values('member_age'))
+
+    print('>>>> 데이터가 모였다', lst1, lst2, lst3)
+
+
+
+
+
+
+
     if request.session.get('member_name') :
         print('>>> our member')
         # 세션을 계속 심어줘야 함
-        lst = []
-
 
         context = {
             'session_member_name' :  request.session.get('member_name'),
             'session_member_id': request.session.get('member_id'),
-            'chart_data' : lst
+            'chart_data1' : lst1,
+            'chart_data2': lst2,
+            'chart_data3': lst3,
+
         }
         return render(request, 'index.html',context)
     else:
-        return render(request, 'index.html')
+        context = {
+            'chart_data1': lst1,
+            'chart_data2': lst2,
+            'chart_data3': lst3,
+        }
+
+        return render(request, 'index.html', context)
 
 
 
